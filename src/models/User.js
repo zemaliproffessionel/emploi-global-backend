787da3bdfaa3063aaafb5ee4d16ead8e90a72a03
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-17 23:38:14 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,7 @@
-// On importe notre connecteur à la base de données
 const db = require('../config/db');
 
 const User = {};
 
-// Fonction pour créer un nouvel utilisateur dans la base de données
 User.create = async (email, hashedPassword) => {
   const query = `
     INSERT INTO users (email, password_hash)
@@ -14,14 +12,25 @@ User.create = async (email, hashedPassword) => {
   
   try {
     const res = await db.query(query, values);
-    return res.rows[0]; // Retourne le nouvel utilisateur créé
+    return res.rows[0];
   } catch (err) {
     console.error(err.stack);
     throw err;
   }
 };
 
-// Plus tard, nous ajouterons ici d'autres fonctions, comme pour trouver un utilisateur
-// User.findByEmail = async (email) => { ... };
+// FONCTION AJOUTÉE : trouver un utilisateur par son email
+User.findByEmail = async (email) => {
+  const query = 'SELECT * FROM users WHERE email = $1';
+  const values = [email];
+  
+  try {
+    const res = await db.query(query, values);
+    return res.rows[0]; // Retourne l'utilisateur s'il est trouvé, sinon undefined
+  } catch (err) {
+    console.error(err.stack);
+    throw err;
+  }
+};
 
 module.exports = User;
