5fa4f0f4b82e60775f148a1b976eea08d0ea009d
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 08:58:55 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
-// Importe le framework Express pour créer le serveur
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // <-- LIGNE AJOUTÉE
+const userRoutes = require('./routes/userRoutes');
+const jobRoutes = require('./routes/jobRoutes'); // <-- LIGNE AJOUTÉE
 
-// Crée l'application Express
 const app = express();
-
-// Configure l'application pour qu'elle puisse communiquer avec le site web (frontend)
 app.use(cors());
-
-// Configure l'application pour qu'elle puisse lire les données JSON
 app.use(express.json());
 
-// Route de test pour vérifier que le serveur fonctionne
 app.get('/', (req, res) => {
   res.send('Le serveur EmploiGlobal fonctionne !');
 });
 
-// Indique à l'application d'utiliser les routes des utilisateurs
-app.use('/api/users', userRoutes); // <-- LIGNE AJOUTÉE
+app.use('/api/users', userRoutes);
+app.use('/api/jobs', jobRoutes); // <-- LIGNE AJOUTÉE
 
-// Exporte l'application pour qu'elle puisse être utilisée par d'autres fichiers
 module.exports = app;
