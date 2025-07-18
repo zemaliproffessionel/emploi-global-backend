42682d0474e7dd6ad2645cd9f716e88d85e05eb3
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 08:49:02 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-// Importe l'application que nous venons de créer
 const app = require('./app');
+const { initScheduledJobs } = require('./services/cronManager'); // <-- LIGNE AJOUTÉE
 
-// Définit le port sur lequel le serveur va écouter. 
-// Par défaut, ce sera le port 5000.
 const PORT = process.env.PORT || 5000;
 
-// Lance le serveur et affiche un message dans la console pour dire qu'il est prêt
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
+  
+  // Lancer le planificateur de tâches
+  initScheduledJobs(); // <-- LIGNE AJOUTÉE
 });
