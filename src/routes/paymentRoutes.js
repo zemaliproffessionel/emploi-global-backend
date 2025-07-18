6d70a2301ac53e280e9a071f0b6895d964065da8
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-18 13:30:05 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createPaymentIntent } = require('../controllers/paymentController');
+const { createPaymentIntent, getAllPayments, validatePayment } = require('../controllers/paymentController');
+// Plus tard, on ajoutera un middleware pour vérifier que seul un admin peut accéder
+// const { protect, admin } = require('../middleware/authMiddleware');
 
+// Route pour l'utilisateur
 router.post('/create-intent', createPaymentIntent);
+
+// Routes pour l'admin
+router.get('/', getAllPayments); // protect, admin, 
+router.put('/:paymentId/validate', validatePayment); // protect, admin, 
 
 module.exports = router;
