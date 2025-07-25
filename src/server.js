c734828eceb58c565b7b163b3520515412f0f709
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-25 19:34:34 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const app = require('./app');
-const { initScheduledJobs } = require('./services/cronManager'); // <-- LIGNE AJOUTÉE
+const { scheduleJobs } = require('./jobScheduler'); // On importe notre nouveau planificateur
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
-  
-  // Lancer le planificateur de tâches
-  initScheduledJobs(); // <-- LIGNE AJOUTÉE
+  // On lance la planification des tâches (qui va remplir la DB si elle est vide)
+  scheduleJobs();
 });
