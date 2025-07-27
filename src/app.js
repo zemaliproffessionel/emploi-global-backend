2af55407f41d0356b53b77e1d01b8529a8d7f6d1
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-07-27 14:56:20 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-const express = require('express');
+const express = require('express' );
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-const jobRoutes = require('./routes/jobRoutes'); // <-- LIGNE AJOUTÉE
-const paymentRoutes = require('./routes/paymentRoutes'); // <-- LIGNE AJOUTÉE
+const jobRoutes = require('./routes/jobRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 const app = express();
-app.use(cors());
+
+// --- Configuration CORS ---
+// On définit l'adresse de notre frontend
+const allowedOrigins = ['https://emploi-global-frontend.onrender.com'];
+
+const corsOptions = {
+  origin: function (origin, callback ) {
+    // On autorise les requêtes qui viennent de notre frontend
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+// On applique la configuration CORS
+app.use(cors(corsOptions));
+// --- Fin de la configuration CORS ---
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -13,6 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', userRoutes);
-app.use('/api/jobs', jobRoutes); // <-- LIGNE AJOUTÉE
-app.use('/api/payments', paymentRoutes); // <-- LIGNE AJOUTÉE
+app.use('/api/jobs', jobRoutes);
+app.use('/api/payments', paymentRoutes);
+
 module.exports = app;
