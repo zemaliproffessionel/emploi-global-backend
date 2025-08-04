802a609c7057b08df74e84fd843939076de81914
zemaliproffessionel <zemaliproffessionel@gmail.com>
2025-08-04 17:16:42 +0100

    Update jobController.js

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -1,4 +1,4 @@
-const Job = require('../models/Job');
+const Job = require("../models/Job");
 
 const getAllJobs = async (req, res) => {
   try {
@@ -7,26 +7,34 @@ const getAllJobs = async (req, res) => {
     const jobs = await Job.getAll(filters);
     res.status(200).json(jobs);
   } catch (error) {
-    res.status(500).json({ message: 'Erreur du serveur lors de la récupération des offres.' });
+    res.status(500).json({ message: "Erreur du serveur lors de la récupération des offres." });
   }
 };
 
-// NOUVELLE FONCTION
 const getJobById = async (req, res) => {
   try {
     const job = await Job.findById(req.params.id);
     if (job) {
       res.status(200).json(job);
     } else {
-      res.status(404).json({ message: 'Offre non trouvée' });
+      res.status(404).json({ message: "Offre non trouvée" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Erreur du serveur.' });
+    res.status(500).json({ message: "Erreur du serveur." });
+  }
+};
+
+const getUniqueCountries = async (req, res) => {
+  try {
+    const countries = await Job.getUniqueCountries();
+    res.status(200).json(countries);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur du serveur lors de la récupération des pays uniques." });
   }
 };
 
 module.exports = {
   getAllJobs,
-  getJobById, // On exporte la nouvelle fonction
+  getJobById,
+  getUniqueCountries, // On exporte la nouvelle fonction
 };
-
